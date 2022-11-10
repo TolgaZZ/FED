83e1c25eac5196898f667a7825326deb307f139d
Tolga Zorlu <tolga.z@outlook.com>
2022-11-10 03:04:15 +0100

    Hamburger Menu added

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,14 @@
 // https://codepen.io/bstonedev/pen/MWWZgKz
 // https://codepen.io/ricardospalves/pen/JpOmMR?editors=1111
 
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
 // JavaScript Document
 console.log("hi");
 // Get all stack__card divs
@@ -27,7 +35,7 @@ function fadeIn() {
 		if (i + 1 < elementsArray.length) {
 			var nextElem = elementsArray[i + 1];
 			if (nextElem.getBoundingClientRect().top <= 0) {
-				// Next element is coving the current element. Make current element invisible
+				// Next element is covering the current element. Make current element invisible
 				elem.classList.add("underView");
 				elem.classList.remove("movingUnderView");
 			} else if (isMovingInView(nextElem)) {
@@ -43,3 +51,14 @@ function fadeIn() {
 	}
 }
 fadeIn();
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
